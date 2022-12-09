3ee417a29176c341433a0c2bf199f19e9a26e6ee
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 00:24:20 +0100

    implemented constructors, getters and setters

--- src/main/java/unsa/etf/rpr/domain/Subscriber.java
+++ src/main/java/unsa/etf/rpr/domain/Subscriber.java
@@ -1,4 +1,34 @@
 package unsa.etf.rpr.domain;
 
-public class Subscriber {
+import java.io.Serializable;
+
+public class Subscriber implements Serializable {
+
+    private int subscriberId;
+    private String preferences;
+
+    public Subscriber() {
+    }
+
+    public Subscriber(int subscriberId, String preferences) {
+        this.subscriberId = subscriberId;
+        this.preferences = preferences;
+    }
+
+    public int getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(int subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
 }
