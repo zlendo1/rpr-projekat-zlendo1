d1405c7c23de951f89e3916f10919980a9dccd52
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-09 16:25:19 +0100

    created App and AppTest

---
+++ src/test/java/unsa/etf/rpr/AppTest.java
@@ -0,0 +1,20 @@
+package unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
