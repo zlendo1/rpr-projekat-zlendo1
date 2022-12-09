6955c6cd791a15b3c0ab120d1cd0cb6c62e55b89
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 00:26:19 +0100

    implemented equals, hashCode and toString

--- src/main/java/unsa/etf/rpr/domain/Subscriber.java
+++ src/main/java/unsa/etf/rpr/domain/Subscriber.java
@@ -1,7 +1,11 @@
 package unsa.etf.rpr.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+/**
+ * Bean for subscriber
+ */
 public class Subscriber implements Serializable {
 
     private int subscriberId;
@@ -29,6 +33,27 @@ public class Subscriber implements Serializable {
 
     public void setPreferences(String preferences) {
         this.preferences = preferences;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subscriber)) return false;
+        Subscriber that = (Subscriber) o;
+        return getSubscriberId() == that.getSubscriberId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSubscriberId());
+    }
+
+    @Override
+    public String toString() {
+        return "Subscriber{" +
+                "subscriberId=" + subscriberId +
+                ", preferences='" + preferences + '\'' +
+                '}';
     }
 
 }
