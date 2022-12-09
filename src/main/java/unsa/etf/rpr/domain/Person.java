5725ac35c7f81f8581a1e0abf83db3cc7201ad56
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-09 23:59:18 +0100

    implmenented constructors, getters and setters

--- src/main/java/unsa/etf/rpr/domain/Person.java
+++ src/main/java/unsa/etf/rpr/domain/Person.java
@@ -1,4 +1,47 @@
 package unsa.etf.rpr.domain;
 
-public class Person {
+import java.io.Serializable;
+
+/**
+ * Bean for person
+ */
+public class Person implements Serializable {
+
+    private int personId;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(int personId, String firstName, String lastName) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
