d25d0eba05605577a1014f0ad7771e8731317984
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 22:51:17 +0100

    implemented dao for Provider

--- src/main/java/unsa/etf/rpr/dao/ProviderDao.java
+++ src/main/java/unsa/etf/rpr/dao/ProviderDao.java
@@ -2,5 +2,20 @@ package unsa.etf.rpr.dao;
 
 import unsa.etf.rpr.domain.Provider;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Dao interface for Provider bean
+ */
 public interface ProviderDao extends Dao<Provider> {
+
+    /**
+     * Search providers in DB based on their contract validity in a time period
+     * @param begin Starting datetime (inclusive)
+     * @param end Ending datetime (inclusive)
+     * @return List of providers
+     */
+    List<Provider> getByDateRange(LocalDateTime begin, LocalDateTime end);
+
 }
