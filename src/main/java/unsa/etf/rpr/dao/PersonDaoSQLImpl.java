4171d07d533283a5b57fc2255247a1d412791f32
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 23:26:21 +0100

    prepared class for implementation

--- src/main/java/unsa/etf/rpr/dao/PersonDaoSQLImpl.java
+++ src/main/java/unsa/etf/rpr/dao/PersonDaoSQLImpl.java
@@ -1,4 +1,44 @@
 package unsa.etf.rpr.dao;
 
+import unsa.etf.rpr.domain.Person;
+
+import java.util.List;
+
 public class PersonDaoSQLImpl implements PersonDao {
+
+    @Override
+    public Person getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Person add(Person item) {
+        return null;
+    }
+
+    @Override
+    public Person update(Person item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Person> searchByFirstName(String firstName) {
+        return null;
+    }
+
+    @Override
+    public List<Person> searchByLastName(String lastName) {
+        return null;
+    }
+
 }
