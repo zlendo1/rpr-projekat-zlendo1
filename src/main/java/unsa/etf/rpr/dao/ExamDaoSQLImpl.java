fbf540212d77c92201779633ad741c8af4ce5075
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 23:49:05 +0100

    prepared class for implementation

--- src/main/java/unsa/etf/rpr/dao/ExamDaoSQLImpl.java
+++ src/main/java/unsa/etf/rpr/dao/ExamDaoSQLImpl.java
@@ -1,4 +1,107 @@
 package unsa.etf.rpr.dao;
 
+import unsa.etf.rpr.domain.Exam;
+import unsa.etf.rpr.domain.Provider;
+
+import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ExamDaoSQLImpl implements ExamDao {
+
+    private Connection connection;
+
+    /**
+     * Establishes connection to the DB
+     *
+     */
+    public ExamDaoSQLImpl() {
+    }
+
+    /**
+     * Get entity from database coresponding to its primary key
+     *
+     * @param id primary key of entity
+     * @return corresponding entity
+     */
+    @Override
+    public Exam getById(int id) {
+        return null;
+    }
+
+    /**
+     * Saves entity into database
+     *
+     * @param item bean for saving into database
+     * @return updated version of the bean
+     */
+    @Override
+    public Exam add(Exam item) {
+        return null;
+    }
+
+    /**
+     * Updates entity from database based on its primary key
+     *
+     * @param item bean which we will update (id must be populated)
+     * @return updated version of the bean
+     */
+    @Override
+    public Exam update(Exam item) {
+        return null;
+    }
+
+    /**
+     * Hard delete of entity with the corseponding primary key
+     *
+     * @param id primary key of the entity
+     */
+    @Override
+    public void delete(int id) {
+
+    }
+
+    /**
+     * Lists all entites from the database
+     *
+     * @return list of entities from the database
+     */
+    @Override
+    public List<Exam> getAll() {
+        return null;
+    }
+
+    /**
+     * Search for exams in DB by provider
+     *
+     * @param provider The provider that provided the exams
+     * @return List of exams
+     */
+    @Override
+    public List<Exam> getByProvider(Provider provider) {
+        return null;
+    }
+
+    /**
+     * Search for exams in DB by course name
+     *
+     * @param courseName Name of the course the exam belongs to
+     * @return List of exams
+     */
+    @Override
+    public List<Exam> getByCourseName(String courseName) {
+        return null;
+    }
+
+    /**
+     * Search for exams in DB by exam time
+     *
+     * @param examTime Date and time of the exam
+     * @return List of exams
+     */
+    @Override
+    public List<Exam> getByExamTime(LocalDateTime examTime) {
+        return null;
+    }
+
 }
