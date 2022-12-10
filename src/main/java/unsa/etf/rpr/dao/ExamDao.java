9f5b7b87a1dc8308b82f110658bb162516f5d72b
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 23:04:41 +0100

    implemented dao interface for Exam bean

--- src/main/java/unsa/etf/rpr/dao/ExamDao.java
+++ src/main/java/unsa/etf/rpr/dao/ExamDao.java
@@ -1,6 +1,35 @@
 package unsa.etf.rpr.dao;
 
 import unsa.etf.rpr.domain.Exam;
+import unsa.etf.rpr.domain.Provider;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Dao interface for Exam bean
+ */
 public interface ExamDao extends Dao<Exam> {
+
+    /**
+     * Search for exams in DB by provider
+     * @param provider The provider that provided the exams
+     * @return List of exams
+     */
+    List<Exam> getByProvider(Provider provider);
+
+    /**
+     * Search for exams in DB by course name
+     * @param courseName Name of the course the exam belongs to
+     * @return List of exams
+     */
+    List<Exam> getByCourseName(String courseName);
+
+    /**
+     * Search for exams in DB by exam time
+     * @param examTime Date and time of the exam
+     * @return List of exams
+     */
+    List<Exam> getByExamTime(LocalDateTime examTime);
+
 }
