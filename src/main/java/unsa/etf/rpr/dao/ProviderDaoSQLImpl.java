9e7aefc164b59e307bc7c6660d854043f0d07374
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 23:26:52 +0100

    prepared class for implementation

--- src/main/java/unsa/etf/rpr/dao/ProviderDaoSQLImpl.java
+++ src/main/java/unsa/etf/rpr/dao/ProviderDaoSQLImpl.java
@@ -1,4 +1,46 @@
 package unsa.etf.rpr.dao;
 
+import unsa.etf.rpr.domain.Provider;
+
+import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ProviderDaoSQLImpl implements ProviderDao {
+
+    private Connection connection;
+
+    public ProviderDaoSQLImpl() {
+    }
+
+    @Override
+    public Provider getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Provider add(Provider item) {
+        return null;
+    }
+
+    @Override
+    public Provider update(Provider item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Provider> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Provider> getByDateRange(LocalDateTime begin, LocalDateTime end) {
+        return null;
+    }
+
 }
