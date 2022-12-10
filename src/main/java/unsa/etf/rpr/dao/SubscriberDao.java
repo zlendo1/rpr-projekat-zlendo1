9d1bf401a7e049165bda943d8a4840aa0336233b
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 22:56:57 +0100

    implemented dao interface for Subscriber

--- src/main/java/unsa/etf/rpr/dao/SubscriberDao.java
+++ src/main/java/unsa/etf/rpr/dao/SubscriberDao.java
@@ -2,5 +2,17 @@ package unsa.etf.rpr.dao;
 
 import unsa.etf.rpr.domain.Subscriber;
 
+import java.util.List;
+
+/**
+ * Dao interface for Subscriber bean
+ */
 public interface SubscriberDao extends Dao<Subscriber> {
+
+    /**
+     * Search subscribers in DB by their exam preferences
+     * @param preference Exam preference (only one)
+     * @return List of subscribers
+     */
+    List<Subscriber> getByPreference(String preference);
 }
