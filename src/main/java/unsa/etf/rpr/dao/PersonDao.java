b9c8874be67e660ee9df7fc57a77c61bc0f76253
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-10 22:25:00 +0100

    implemented bean-specific methods

--- src/main/java/unsa/etf/rpr/dao/PersonDao.java
+++ src/main/java/unsa/etf/rpr/dao/PersonDao.java
@@ -2,5 +2,23 @@ package unsa.etf.rpr.dao;
 
 import unsa.etf.rpr.domain.Person;
 
-public interface PersonDao extends Dao<Person>{
+import java.util.List;
+
+/**
+ * Dao interface for Person bean
+ */
+public interface PersonDao extends Dao<Person> {
+
+    /**
+     * @param firstName First name of a person
+     * @return List of people with the same first name
+     */
+    List<Person> searchByFirstName(String firstName);
+
+    /**
+     * @param lastName Last name of a person
+     * @return List of people with the same last name
+     */
+    List<Person> searchByLastName(String lastName);
+
 }
