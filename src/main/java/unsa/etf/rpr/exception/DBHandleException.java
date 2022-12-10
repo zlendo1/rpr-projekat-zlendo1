14f56c898b5568239cebbb7b84d6bc837f546386
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-11 00:42:01 +0100

    implemented my exception

---
+++ src/main/java/unsa/etf/rpr/exception/DBHandleException.java
@@ -0,0 +1,25 @@
+package unsa.etf.rpr.exception;
+
+/**
+ * My exception class for handling dao exceptions
+ *
+ */
+public class DBHandleException extends Exception {
+
+    /**
+     * Constructs a new exception with the detail message
+     * @param message - Detail message
+     */
+    public DBHandleException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with given cause
+     * @param cause - Cause of exception
+     */
+    public DBHandleException(Throwable cause) {
+        super(cause);
+    }
+
+}
