8f159e55dba2366ba61c53522ea2334d9a328b6a
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-11 23:13:43 +0100

    implemented getExpired

--- src/main/java/unsa/etf/rpr/dao/SubscriptonDaoSQLImpl.java
+++ src/main/java/unsa/etf/rpr/dao/SubscriptonDaoSQLImpl.java
@@ -259,8 +259,32 @@ public class SubscriptonDaoSQLImpl implements SubscriptionDao {
      * @return List of subscriptions
      */
     @Override
-    public List<Subscription> getExpired() {
-        return null;
+    public List<Subscription> getExpired() throws DBHandleException {
+        List<Subscription> subscriptionList = new ArrayList<>();
+
+        String query = "SELECT * FROM subscription WHERE exporation <= (SELECT sysdate FROM dual)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                subscriptionList.add(new Subscription(
+                        resultSet.getInt("subscription_id"),
+                        new SubscriberDaoSQLImpl().getById(resultSet.getInt("subscriber_id")),
+                        new ExamDaoSQLImpl().getById(resultSet.getInt("exam_id")),
+                        resultSet.getTimestamp("exporation")
+                ));
+            }
+
+            resultSet.close();
+
+        } catch (Exception e) {
+            throw new DBHandleException(e);
+        }
+
+        return subscriptionList;
     }
 
     /**
