d4c2bf4ff20b769410427c2811f4908a37715121
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-17 22:33:17 +0100

    added login controller class

---
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public Button cancelButton;
+    public Button registerButton;
+    public Button continueButton;
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void registerButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void continueButtonAction(ActionEvent actionEvent) {
+    }
+}
