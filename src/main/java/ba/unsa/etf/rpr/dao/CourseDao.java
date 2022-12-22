65ccac9c62887ec04a1f53fd0ee26e96598cabe3
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-22 21:18:51 +0100

    created bean specific interface methods

--- src/main/java/ba/unsa/etf/rpr/dao/CourseDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CourseDao.java
@@ -1,6 +1,26 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.exception.DBHandleException;
+
+import java.util.List;
 
 public interface CourseDao extends Dao<Course> {
+
+    /**
+     * Search for courses in DB by its name
+     *
+     * @param name Course name
+     * @return List of courses
+     */
+    List<Course> searchByName(String name) throws DBHandleException;
+
+    /**
+     * Search for courses in DB by its professor's name
+     *
+     * @param name A professor's full name
+     * @return List of courses
+     */
+    List<Course> searchByProfessor(String name) throws DBHandleException;
+
 }
