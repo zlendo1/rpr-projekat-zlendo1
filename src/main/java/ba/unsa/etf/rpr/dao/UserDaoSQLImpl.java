c85a28bd349315321e233358d7fc0655960d8819
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-22 22:15:05 +0100

    reformatted impl and imported methods

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -7,183 +7,16 @@ import ba.unsa.etf.rpr.exception.DBHandleException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * SQL implementation of UserDao
  *
  */
-public class UserDaoSQLImpl implements UserDao {
+public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
 
-    private final Connection connection;
-
-    /**
-     * Establishes connection to the DB
-     *
-     */
     public UserDaoSQLImpl() throws DBHandleException {
-        connection = MyConnection.getInstance().getConnection();
-    }
-
-    /**
-     * Get entity from database coresponding to its primary key
-     *
-     * @param id primary key of entity
-     * @return corresponding entity
-     * @throws DBHandleException In case of any DB handling error
-     */
-    @Override
-    public User getById(int id) throws DBHandleException {
-        String query = "SELECT * FROM user WHERE user_id = ?";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-            preparedStatement.setInt(1, id);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                User user = new User(
-                        resultSet.getInt("user_id"),
-                        resultSet.getString("username"),
-                        resultSet.getString("password"),
-                        resultSet.getString("first_name"),
-                        resultSet.getString("last_name")
-                );
-
-                resultSet.close();
-
-                return user;
-            }
-
-        } catch (SQLException e) {
-            throw new DBHandleException(e);
-        }
-
-        return null;
-    }
-
-    /**
-     * Saves entity into database
-     *
-     * @param item bean for saving into database
-     * @return updated version of the bean
-     * @throws DBHandleException In case of any DB handling error
-     */
-    @Override
-    public User add(User item) throws DBHandleException {
-        String insert = "INSERT INTO user(first_name, last_name) VALUES(?, ?)";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-
-            preparedStatement.setString(1, item.getFirstName());
-            preparedStatement.setString(2, item.getLastName());
-
-            preparedStatement.executeUpdate();
-
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-
-            resultSet.next();
-
-            item.setId(resultSet.getInt(1));
-
-            resultSet.close();
-
-            return item;
-
-        } catch (SQLException e) {
-            throw new DBHandleException(e);
-        }
-    }
-
-    /**
-     * Updates entity from database based on its primary key
-     *
-     * @param item bean which we will update (id must be populated)
-     * @return updated version of the bean
-     * @throws DBHandleException In case of any DB handling error
-     */
-    @Override
-    public User update(User item) throws DBHandleException {
-        String update = "UPDATE user SET first_name = ?, last_name = ? WHERE user_id = ?";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(update);
-
-            preparedStatement.setInt(3, item.getId());
-            preparedStatement.setString(1, item.getFirstName());
-            preparedStatement.setString(2, item.getLastName());
-
-            preparedStatement.executeUpdate();
-
-            preparedStatement.close();
-
-            return item;
-
-        } catch (SQLException e) {
-            throw new DBHandleException(e);
-        }
-    }
-
-    /**
-     * Hard delete of entity with the corseponding primary key
-     *
-     * @param id primary key of the entity
-     * @throws DBHandleException In case of any DB handling error
-     */
-    @Override
-    public void delete(int id) throws DBHandleException {
-        String delete = "DELETE FROM user WHERE user_id = ?";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(delete);
-
-            preparedStatement.setInt(1, id);
-
-            preparedStatement.executeUpdate();
-
-            preparedStatement.close();
-
-        } catch (SQLException e) {
-            throw new DBHandleException(e);
-        }
-    }
-
-    /**
-     * Lists all entites from the database
-     *
-     * @return list of entities from the database
-     * @throws DBHandleException In case of any DB handling error
-     */
-    @Override
-    public List<User> getAll() throws DBHandleException {
-        List<User> userList = new ArrayList<>();
-
-        String query = "SELECT * FROM user";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                userList.add(new User(
-                        resultSet.getInt("user_id"),
-                        resultSet.getString("username"),
-                        resultSet.getString("password"),
-                        resultSet.getString("first_name"),
-                        resultSet.getString("last_name"))
-                );
-            }
-
-            resultSet.close();
-
-        } catch (SQLException e) {
-            throw new DBHandleException(e);
-        }
-
-        return userList;
+        super("user");
     }
 
     /**
@@ -200,7 +33,7 @@ public class UserDaoSQLImpl implements UserDao {
         String query = "SELECT * FROM user WHERE first_name = ?";
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
 
             preparedStatement.setString(1, firstName);
 
@@ -239,7 +72,7 @@ public class UserDaoSQLImpl implements UserDao {
         String query = "SELECT * FROM user WHERE last_name = ?";
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
 
             preparedStatement.setString(1, lastName);
 
@@ -278,7 +111,7 @@ public class UserDaoSQLImpl implements UserDao {
         String query = "SELECT * FROM user WHERE username = ?";
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
 
             preparedStatement.setString(1, username);
 
@@ -317,7 +150,7 @@ public class UserDaoSQLImpl implements UserDao {
         String query = "SELECT * FROM user WHERE password = ?";
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
 
             preparedStatement.setString(1, password);
 
@@ -340,6 +173,30 @@ public class UserDaoSQLImpl implements UserDao {
         }
 
         return userList;
+    }
+
+    /**
+     * Extracts a bean from a ResultSet object.
+     *
+     * @param resultSet Contains the row we want to extract
+     * @return Bean object
+     * @throws DBHandleException In case of errors while working with ResultSet
+     */
+    @Override
+    public User rowToObject(ResultSet resultSet) throws DBHandleException {
+        return null;
+    }
+
+    /**
+     * Get row utilising a Map object from a bean object.
+     * The keys are the names of the bean's attributes.
+     *
+     * @param object Bean object which we want converted
+     * @return Map object containing bean
+     */
+    @Override
+    public Map<String, Object> objectToRow(User object) {
+        return null;
     }
 
 }
