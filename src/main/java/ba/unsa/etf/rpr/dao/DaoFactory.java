d16870f09a50f40fccccc01102359419d9b58ea4
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-22 22:46:05 +0100

    implemented DaoFactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,8 +1,41 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exception.DBHandleException;
+
 /**
  * Singleton factory for Dao objects
  *
  */
 public class DaoFactory {
+
+    private static UserDao userDao = null;
+    private static CourseDao courseDao = null;
+    private static ExamDao examDao = null;
+
+    private DaoFactory() {}
+
+    public static UserDao userDao() throws DBHandleException {
+        if (userDao == null) {
+            userDao = new UserDaoSQLImpl();
+        }
+
+        return userDao;
+    }
+
+    public static CourseDao courseDao() throws DBHandleException {
+        if (courseDao == null) {
+            courseDao = new CourseDaoSQLImpl();
+        }
+
+        return courseDao;
+    }
+
+    public static ExamDao examDao() throws DBHandleException {
+        if (examDao == null) {
+            examDao = new ExamDaoSQLImpl();
+        }
+
+        return examDao;
+    }
+
 }
