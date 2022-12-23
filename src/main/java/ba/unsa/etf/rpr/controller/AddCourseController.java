554badb701f2f45b9dee8b9c72faf9d92e652852
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-23 22:21:19 +0100

    added necessary attributes and methods

--- src/main/java/ba/unsa/etf/rpr/controller/AddCourseController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/AddCourseController.java
@@ -1,8 +1,27 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 /**
  * Controller class for the course addition scene.
  *
  */
 public class AddCourseController {
+
+    public TextField courseNameField;
+    public TextField professorField;
+    public Text courseNameErrorField;
+    public Text professorErrorField;
+    public Button addButton;
+    public Button cancelButton;
+
+    public void addButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+
 }
