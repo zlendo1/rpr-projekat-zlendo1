45ed5aca3d6edcb08ce0b380a207b1b4f0cffa1e
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-23 19:24:52 +0100

    created visual attributes and controller methods

--- src/main/java/ba/unsa/etf/rpr/controller/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/RegistrationController.java
@@ -1,4 +1,64 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class RegistrationController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public PasswordField confirmPasswordField;
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public Text usernameErrorField;
+    public Text passwordErrorField;
+    public Text confirmPasswordErrorField;
+    public Text firstNameErrorField;
+    public Text lastNameErrorField;
+    public Button cancelButton;
+    public Button registerButton;
+    public Button registerAndLoginButton;
+
+    public void usernameFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void passwordFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void confirmPasswordFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void firstNameFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void lastNameFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void usernameErrorFieldAction(MouseEvent mouseEvent) {
+    }
+
+    public void passwordErrorFieldAction(MouseEvent mouseEvent) {
+    }
+
+    public void confirmPasswordErrorFieldAction(MouseEvent mouseEvent) {
+    }
+
+    public void firstNameErrorFieldAction(MouseEvent mouseEvent) {
+    }
+
+    public void lastNameErrorFieldAction(MouseEvent mouseEvent) {
+    }
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void registerButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void registerAndLoginButtonAction(ActionEvent actionEvent) {
+    }
+
 }
