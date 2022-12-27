d7e0d8469dc56a6311586283b001e0f95ba0103d
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-27 22:47:05 +0100

    defined attributes and methods that will be necessary for the control class

--- src/main/java/ba/unsa/etf/rpr/controller/AddExamController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/AddExamController.java
@@ -1,8 +1,29 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
 /**
  * Controller class for the exam addition scene.
  *
  */
 public class AddExamController {
+
+    public TextArea answerSheetField;
+    public Text courseNameFieldError;
+    public Text examTimeFieldError;
+    public Text answerSheetFieldError;
+    public DatePicker examTimeField;
+    public Button cancelButton;
+    public Button addButton;
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void addButtonAction(ActionEvent actionEvent) {
+    }
+
 }
