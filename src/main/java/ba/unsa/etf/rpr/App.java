d04ef1214aab0554d8eb55a345d633aa9696f205
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-29 17:22:57 +0100

    streamlined scene loading

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,14 +1,8 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.controller.SceneLoader;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.util.Objects;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 /**
  * Hello world!
@@ -22,13 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
-
-        stage.setTitle("Login");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-
-        stage.show();
+        SceneLoader.load(stage, "login", "Login", false);
     }
 
 }
