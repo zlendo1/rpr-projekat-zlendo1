718e2c2a4294c32752ac98108ebf5815dd1b85c7
zlendo1 <zlendo1@etf.unsa.ba>
2022-12-29 17:21:12 +0100

    created documentation for the load method

--- src/main/java/ba/unsa/etf/rpr/controller/SceneLoader.java
+++ src/main/java/ba/unsa/etf/rpr/controller/SceneLoader.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -14,8 +13,19 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
  * Auxiliary class for stage loading.
  *
  */
-public class StageLoader {
+public class SceneLoader {
 
+    /**
+     * A simple loader for scenes.
+     * Implemeneted to avoid spaghetification/repetition.
+     *
+     * @param stage Stage object on which the scene will be set
+     * @param fileName Name of the scene's file
+     *                 Must be located in the resources/fxml/ and cannot have the extention included
+     * @param titleName Title of the scene
+     * @param resizable Boolean value for resizeability
+     * @throws IOException In case the fileName is formated incorrectly or the file does not exist
+     */
     public static void load(Stage stage, String fileName, String titleName, boolean resizable) throws IOException {
         Parent root = FXMLLoader.load(
                 ClassLoader.getSystemResource("/fxml/" + fileName + ".fxml")
