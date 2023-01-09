cc1dbab494be5f2cbe8193ebf9565de684270552
zlendo1 <zlendo1@etf.unsa.ba>
2023-01-09 14:31:16 +0100

    created and implemented throwAlert method

--- src/main/java/ba/unsa/etf/rpr/auxiliary/AlertThrower.java
+++ src/main/java/ba/unsa/etf/rpr/auxiliary/AlertThrower.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rpr.auxiliary;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 /**
  * A simple class with a static method used for throwing alerts easier with exceptions.
  *
  */
 public class AlertThrower {
+
+    public static void throwAlert(Exception e, Alert.AlertType alertType) {
+        new Alert(alertType, e.getMessage(), ButtonType.OK).show();
+    }
+
 }
