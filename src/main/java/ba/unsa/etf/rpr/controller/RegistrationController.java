f71fd276da97706f469ec7ed0fbc8a31574d77d6
zlendo1 <zlendo1@etf.unsa.ba>
2023-01-09 14:20:19 +0100

    implemented cancel method which returns us to the login screen

--- src/main/java/ba/unsa/etf/rpr/controller/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/RegistrationController.java
@@ -1,11 +1,17 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.auxiliary.SceneLoader;
 import ba.unsa.etf.rpr.business.UserManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Controller class for the registration scene.
@@ -33,6 +39,13 @@ public class RegistrationController {
     }
 
     public void cancel(ActionEvent actionEvent) {
+        Stage stage = (Stage) usernameField.getScene().getWindow();
+
+        try {
+            SceneLoader.load(stage, "/fxml/login.fxml", "Login", false);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
 
     public void register(ActionEvent actionEvent) {
