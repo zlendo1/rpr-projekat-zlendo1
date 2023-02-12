01410b3c2884cae9c8ef742a494d6603921948c9
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-12 18:34:08 +0100

    implemented table initialization

--- src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
@@ -2,14 +2,19 @@ package ba.unsa.etf.rpr.controller;
 
 import ba.unsa.etf.rpr.business.ExamManager;
 import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.domain.Exam;
 import ba.unsa.etf.rpr.domain.User;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Controller class for the home screen.
@@ -20,11 +25,12 @@ public class HomeController {
     // GUI objects
     public TextField courseNameSearch;
     public DatePicker examTimeSearch;
-    public TableColumn<User, String> usernameColumn;
-    public TableColumn<Course, String> courseNameColumn;
-    public TableColumn<Course, String> professorColumn;
-    public TableColumn<Course, Date> examTimeColumn;
-    public TableColumn<Course, String> answerSheetColumn;
+    public TableView<Exam> table;
+    public TableColumn<Exam, String> usernameColumn;
+    public TableColumn<Exam, String> courseNameColumn;
+    public TableColumn<Exam, String> professorColumn;
+    public TableColumn<Exam, Date> examTimeColumn;
+    public TableColumn<Exam, String> answerSheetColumn;
 
     // Manager objects
     private final ExamManager manager = new ExamManager();
@@ -38,6 +44,15 @@ public class HomeController {
 
     @FXML
     public void initialize() {
+        List<Exam> examList = manager.searchExam(null, null);
+
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        courseNameColumn.setCellValueFactory(new PropertyValueFactory<>("courseName"));
+        professorColumn.setCellValueFactory(new PropertyValueFactory<>("professor"));
+        examTimeColumn.setCellValueFactory(new PropertyValueFactory<>("examTime"));
+        answerSheetColumn.setCellValueFactory(new PropertyValueFactory<>("answerSheet"));
+
+        table.setItems(FXCollections.observableList(examList));
     }
 
     public void searchExams(ActionEvent actionEvent) {
