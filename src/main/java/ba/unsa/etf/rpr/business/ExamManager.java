ece9246523b5c9e9bd3296f81266ab3e56acda15
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-12 17:35:42 +0100

    added searchExam functionality

--- src/main/java/ba/unsa/etf/rpr/business/ExamManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ExamManager.java
@@ -1,8 +1,41 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.auxiliary.AlertThrower;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Exam;
+import ba.unsa.etf.rpr.exception.DBHandleException;
+import javafx.scene.control.Alert;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Business layer manager for Exam.
  *
  */
 public class ExamManager {
+
+    public List<Exam> searchExam(String courseName, Date date) {
+        try {
+            List<Exam> exams = DaoFactory.examDao().getAll();
+
+            for (Exam exam : exams) {
+                if (courseName != null && !exam.getCourse().getName().equals(courseName)) {
+                    exams.remove(exam);
+                }
+
+                if (date != null && !exam.getExamTime().equals(date)) {
+                    exams.remove(exam);
+                }
+            }
+
+            return exams;
+        } catch (DBHandleException e) {
+            AlertThrower.throwAlert(e, Alert.AlertType.ERROR);
+        }
+
+        return null;
+    }
+
 }
