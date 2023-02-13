d8d816de6b69a9a0a0426d21ee8f35ec9d657138
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-13 17:43:28 +0100

    added createCourse method

--- src/main/java/ba/unsa/etf/rpr/business/CourseManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CourseManager.java
@@ -1,8 +1,38 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.auxiliary.AlertThrower;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.exception.DBHandleException;
+import javafx.scene.control.Alert;
+
 /**
  * Business layer manager for Course.
  *
  */
 public class CourseManager {
+
+    public Course createCourse(String courseName, String professor) {
+        if (courseName.isEmpty() || professor.isEmpty()) {
+            return null;
+        }
+
+        try {
+            if (DaoFactory.courseDao().searchByName(courseName) != null) {
+                new Alert(Alert.AlertType.ERROR, "Course name occupied");
+            }
+
+            Course course = new Course();
+
+            course.setName(courseName);
+            course.setProfessor(professor);
+
+            return DaoFactory.courseDao().add(course);
+        } catch (DBHandleException e) {
+            AlertThrower.throwAlert(e, Alert.AlertType.ERROR);
+        }
+
+        return null;
+    }
+
 }
