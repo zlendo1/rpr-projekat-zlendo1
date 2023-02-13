2fa3885b88e5b2f367dcdfac55fbacb58444ca70
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-13 17:23:33 +0100

    implemented createExam method

--- src/main/java/ba/unsa/etf/rpr/business/ExamManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ExamManager.java
@@ -2,9 +2,11 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.auxiliary.AlertThrower;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Course;
 import ba.unsa.etf.rpr.domain.Exam;
 import ba.unsa.etf.rpr.exception.DBHandleException;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.util.Date;
 import java.util.List;
@@ -30,6 +32,32 @@ public class ExamManager {
             }
 
             return exams;
+        } catch (DBHandleException e) {
+            AlertThrower.throwAlert(e, Alert.AlertType.ERROR);
+        }
+
+        return null;
+    }
+
+    public Exam createExam(String courseName, Date date, String answerSheet) {
+        if (courseName.isEmpty() || date == null) {
+            return null;
+        }
+
+        try {
+            Course course = DaoFactory.courseDao().searchByName(courseName);
+
+            if (course == null) {
+                new Alert(Alert.AlertType.ERROR, "Given course does not exist", ButtonType.OK);
+            }
+
+            Exam exam = new Exam();
+
+            exam.setCourse(course);
+            exam.setExamTime(date);
+            exam.setAnswerSheet(answerSheet);
+
+            return exam;
         } catch (DBHandleException e) {
             AlertThrower.throwAlert(e, Alert.AlertType.ERROR);
         }
