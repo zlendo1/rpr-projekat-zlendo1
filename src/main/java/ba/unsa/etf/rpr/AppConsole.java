ba368b78a856af7e8f9c4509bb3bf84f5fadad06
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-15 18:45:05 +0100

    added Option object instances

--- src/main/java/ba/unsa/etf/rpr/AppConsole.java
+++ src/main/java/ba/unsa/etf/rpr/AppConsole.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import org.apache.commons.cli.*;
+
 /**
  * Command line interface for our app.
  * Only available to administrators.
@@ -7,4 +9,9 @@ package ba.unsa.etf.rpr;
  *
  */
 public class AppConsole {
+
+    private static final Option addCourse = new Option("c", "add-course", false, "Adding course to the database");
+    private static final Option addExam = new Option("e", "add-exam", false, "Adding exam to the database");
+    private static final Option getExams = new Option("getE", "get-exams", false, "Printing all exam solutions from the database");
+
 }
