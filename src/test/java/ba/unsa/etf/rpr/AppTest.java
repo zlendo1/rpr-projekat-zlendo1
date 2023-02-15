f76a0022cd76e597eebeacac5121b93b44d9c256
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-15 23:37:28 +0100

    removed unnecessary clutter-test

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -7,14 +7,5 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
 }
