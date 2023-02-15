51d59fa26e8345dfe3d4b8454ccd53c4981eb438
zlendo1 <zlendo1@etf.unsa.ba>
2023-02-15 18:52:10 +0100

    implemented printFormattedOptions method

--- src/main/java/ba/unsa/etf/rpr/AppConsole.java
+++ src/main/java/ba/unsa/etf/rpr/AppConsole.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr;
 
 import org.apache.commons.cli.*;
 
+import java.io.PrintWriter;
+
 /**
  * Command line interface for our app.
  * Only available to administrators.
@@ -13,5 +15,15 @@ public class AppConsole {
     private static final Option addCourse = new Option("c", "add-course", false, "Adding course to the database");
     private static final Option addExam = new Option("e", "add-exam", false, "Adding exam to the database");
     private static final Option getExams = new Option("getE", "get-exams", false, "Printing all exam solutions from the database");
+
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+
+        helpFormatter.printUsage(printWriter, 150, "java -jar rpr-projekat-zlendo1.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+
+        printWriter.close();
+    }
 
 }
